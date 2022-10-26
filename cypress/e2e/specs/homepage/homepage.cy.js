dd8ad8102d53a36e2e6397fcc508630024e93e8a
Lucas Santos <LuSantosatl@gmail.com>
2022-10-26 19:49:37 +0200

    first task complete

--- cypress/e2e/specs/homepage/homepage.cy.js
+++ cypress/e2e/specs/homepage/homepage.cy.js
@@ -6,5 +6,7 @@ describe('Test ability to log in, select item, and add to cart.', () => {
     it('Add an itme to the cart ', () => {
         homePagePO.visitPage();
         cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .thumbnail > .pricetag > .productcart').click()
+        cy.get('.block_7 > .nav > .dropdown > .dropdown-toggle').click();
+        cy.get('tbody > :nth-child(2) > :nth-child(2) > a').contains('Skinsheen Bronzer Stick');
     });
 })
